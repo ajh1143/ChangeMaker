5d14f2721c0ef85111b23a47c6eff5efa2f3f36e
Andrew Holt <ajh.206@outlook.com>
2018-12-17 18:16:38 -0800

    Update ChangeMaker.py

--- ChangeMaker.py
+++ ChangeMaker.py
@@ -7,18 +7,28 @@ def make_change(denoms, value):
     :param value: total change due 
     :return: Counter dict of denominations to achieve total value
     """
+    #Create copy of input denomination/coin list
     denoms = denoms.copy()
+    #Create empty list of used coins
     denom_list = []
+    #While value above 0 and denom length above 0
     while value >= 0 and len(denoms) > 0:
+        #Set max coin value
         max_denom = max(denoms)
+        #Check if coin too large to use
         if value - max_denom >= 0:
+            #Add used coin to list of used coins
             denom_list.append(max_denom)
             #print("Adding" + str(max_denom))
+            #Set new value after subtracting used coin
             value = (value - max_denom)
+        #If coin too large to be used, remove from list
         else:
             #print("Removing" + str(max_denom))
             denoms.remove(max_denom)
+    # Generate Counter() of coins used
     coins = Counter(denom_list)
+    # Return Counter() of coins used
     return coins
 
 def plot_change(coinery):
@@ -26,11 +36,17 @@ def plot_change(coinery):
     :param coinery: Counter dict of denominations and counts
     Generate Plot of denominations by count w/ title and axis labels
     """
+    # Generate coin type and value
     labels, values = coinery.items()
+    # Generate bar plot using labels and values
     plt.bar(labels, values)
+    # Add title to plot
     plt.title("Change Due To Customer")
+    # Add x-axis label
     plt.xlabel("Coin Types")
+    # Add y-axis label and rotate horizontally 
     plt.ylabel("Counts").set_rotation(0)
+    # Show plot
     plt.show()
 
 def print_change(coinery):
@@ -38,9 +54,12 @@ def print_change(coinery):
     :param coinery: Counter dict of denominations and counts
     Prints k,v pairs of denomination type, counts, and total value
     """
-    x=[{"Coin Value": k, "Amount": v} for k, v in coinery.items()]
-    for each in x:
+    # Create k,v pairs of coin value and amount
+    coins_used =[{"Coin Value": k, "Amount": v} for k, v in coinery.items()]
+    # For each coin used, print coin and amount
+    for each in coins_used:
         print(each)
+    # Break to new line
     print("\n")
 
 if __name__ == "__main__":
